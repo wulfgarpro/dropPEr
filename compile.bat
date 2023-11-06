d64629f51282a6d479377a1f293cb98eae691812
James Fraser <wulfgar.pro@gmail.com>
2023-11-06 15:12:43 +1100

    Add Visual C++ project
    Template files copied from SEKTOR7's RED TEAM Operator: Malware
    Development Essentials Course, and added to Visual C++ project.

---
+++ compile.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+rc resource.rc
+cvtres /MACHINE:x64 /OUT:resource.o resource.res
+cl.exe /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tcimplant.cpp /link /OUT:implant.exe /SUBSYSTEM:CONSOLE /MACHINE:x64 resource.o
