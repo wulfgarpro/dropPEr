83ae18b04d20d239659542a6ca679243a4407822
James Fraser <wulfgar.pro@gmail.com>
2023-11-07 07:14:57 +1100

    Support bytes and str in xorencrypt.py

--- xorencrypt.py
+++ xorencrypt.py
@@ -8,20 +8,20 @@ import sys
 KEY = "mysecretkeee"
 
 def xor(data, key):
-    key = str(key)
+    key = str(key).encode()
     l = len(key)
     output_bytes = bytearray()
 
     for i in range(len(data)):
-        current = data[i]
+        current = data[i] if isinstance(data, bytes) else ord(data[i])
         current_key = key[i % l]
-        output_bytes.append(current ^ ord(current_key))
+        output_bytes.append(current ^ current_key)
 
     return output_bytes
 
-def printCiphertext(ciphertext):
-	print('{ 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };')
 
+def printCiphertext(ciphertext):
+    print('{ ' + ', '.join('0x{{:02x}'.format(x) for x in ciphertext) + ' };')
 
 
 try:
@@ -30,10 +30,9 @@ except:
     print("File argument needed! %s <raw payload file>" % sys.argv[0])
     sys.exit()
 
-
 ciphertext = xor(plaintext, KEY)
 print('{ 0x' + ', 0x'.join(hex(x)[2:] for x in ciphertext) + ' };')
 
 filename = "favicon.ico"
 with open(filename, "wb") as icon:
-    icon.write(bytes(ciphertext))
+    icon.write(ciphertext)
